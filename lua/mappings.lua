777d66b068727d886829ae0bd67b534ad87c165a
vu.nl <vu.nl@gianty.com.vn>
2025-05-01 23:34:11 +0700

    vietnamese keymap & better mappings for harpoon

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,6 @@
 local map = vim.keymap.set
 
-map('n', '<leader>zz', '<cmd>LspRestart<cr>', { desc = 'Hey, Sleep [zz]zzzz, wake up' })
+map('n', '<leader>zz', '<cmd>LspRestart<CR>', { desc = 'Hey, Sleep [zz]zzzz, wake up' })
 
 -- mark then join then back to the mark
 map('n', 'J', 'mzJ`z')
@@ -12,43 +12,33 @@ map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- TIP: Disable arrow keys in normal mode
 map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-map('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
-map('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
-map('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
-map('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
-
 -- greatest remap ever
 -- unnecessary stuff to the void
 map('x', '<leader>p', [["_dP]])
 
-map('n', '<leader>me', '<cmd>set mouse=a<CR>', { desc = 'Mouse Enable' })
-map('n', '<leader>md', '<cmd>set mouse=<CR>', { desc = 'Mouse Disable' })
+map('n', '<leader>me', '<cmd>set mouse=a<CR>', { desc = '[M]ouse [E]nable' })
+map('n', '<leader>md', '<cmd>set mouse=<CR>', { desc = '[M]ouse [D]isable' })
 map('i', 'jk', '<ESC>', { desc = '<ESC> Escape Insert mode' })
 
 -- Formatter
 map('n', '<leader>fme', ':FormatEnable<CR>')
 map('n', '<leader>fmd', ':FormatDisable<CR>')
 
-map('n', '<leader>ft', ':echo &filetype<CR>', { desc = 'Show current filetype' })
+map('n', '<leader>ft', ':echo &filetype<CR>', { desc = 'Show current [F]ile[T]ype' })
 
 map('n', '<C-w>C', ":exe (v:count ? v:count : '') . 'tabclose'<CR>", { desc = '[C]lose current tab' })
 map('n', '<C-w>O', ":exe (v:count ? v:count : '') . 'tabonly'<CR>", { desc = 'Keep [O]nly other tab' })
 
 -- Set column numbers
-map('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'toggle line number' })
-map('n', '<leader>rn', '<cmd>set rnu!<CR>', { desc = 'toggle relative number' })
+map('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'toggle line [N]umber' })
+map('n', '<leader>rn', '<cmd>set rnu!<CR>', { desc = 'toggle [R]elative [N]umber' })
+
+-- Vietnamese keymap
+map('n', '<leader>kv', '<cmd>set keymap=vietnamese-telex_utf-8<CR>', { desc = 'Set [V]ietnamese [K]eymap' })
+map('n', '<leader>kn', '<cmd>set keymap=""<CR>', { desc = 'Set [N]ormal [K]eymap' })
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -49,60 +49,60 @@ return {
           desc = 'Harpoon [Q]uick Menu',
         },
         {
-          '<C-1>',
+          '<C-h>',
           function()
             harpoon:list():select(1)
           end,
           desc = 'Harpoon Select 1',
         },
         {
-          '<C-2>',
+          '<C-j>',
           function()
             harpoon:list():select(2)
           end,
           desc = 'Harpoon Select 2',
         },
         {
-          '<C-3>',
+          '<C-k>',
           function()
             harpoon:list():select(3)
           end,
           desc = 'Harpoon Select 3',
         },
         {
-          '<C-4>',
+          '<C-l>',
           function()
             harpoon:list():select(4)
           end,
           desc = 'Harpoon Select 4',
         },
         {
-          '<C-5>',
+          '<C-S-h>',
           function()
             harpoon:list():select(5)
           end,
           desc = 'Harpoon Select 5',
         },
         {
-          '<C-6>',
+          '<C-S-j>',
           function()
             harpoon:list():select(6)
           end,
           desc = 'Harpoon Select 6',
         },
         {
-          '<C-7>',
+          '<C-S-k>',
           function()
             harpoon:list():select(7)
           end,
           desc = 'Harpoon Select 7',
         },
         {
-          '<C-[>',
+          '<C-S-l>',
           function()
-            harpoon:list():prev()
+            harpoon:list():select(8)
           end,
-          desc = 'Harpoon Prev',
+          desc = 'Harpoon Select 8',
         },
         {
           '<C-]>',
