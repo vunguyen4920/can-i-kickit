b835c9027463ee90bd514a509cd068870174845e
Vu Nguyen <vunguyen4920@gmail.com>
2025-05-22 22:15:52 +0700

    feat(ui): add harpoon marks to statusline
    This commit adds harpoon marks to the statusline.
    It defines new highlight groups for the active and inactive harpoon marks,
    the harpoon icon, and the separator. It also adds a new module to the statusline
    that displays the harpoon marks.
    Finally, it removes the harpoon marks from the incline plugin.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -29,6 +29,27 @@ M.base46 = {
     'trouble',
     'vim-illuminate',
   },
+
+  hl_add = {
+    St_harpoon_active = {
+      fg = 'yellow',
+      bg = 'one_bg3',
+      bold = true,
+    },
+    St_harpoon_inactive = {
+      fg = 'white',
+      bg = 'one_bg',
+    },
+    St_harpoon_icon = {
+      fg = 'black',
+      bg = 'yellow',
+      bold = true,
+    },
+    St_harpoon_sep = {
+      fg = 'yellow',
+      bg = 'black',
+    },
+  },
 }
 
 M.ui = {
@@ -43,6 +64,38 @@ M.ui = {
   statusline = {
     theme = 'minimal',
     separator_style = 'round',
+    order = { 'mode', 'file', 'git', '%=', 'lsp_msg', '%=', 'diagnostics', 'lsp', 'harpoon_marks', 'cwd', 'cursor' },
+    modules = {
+      harpoon_marks = function()
+        local harpoon = require 'harpoon'
+        if not harpoon or not harpoon.list or not harpoon:list().items then
+          return ''
+        end
+
+        local marks = harpoon:list().items
+        local stbuf = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+        local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(stbuf), ':.')
+        local icon = '󰛢'
+
+        if #marks == 0 then
+          return ''
+        end
+
+        local label = ''
+
+        for id, item in ipairs(marks) do
+          if item.value == path then
+            label = label .. '%#St_harpoon_active# ' .. id .. ' %*'
+          else
+            label = label .. '%#St_harpoon_inactive# ' .. id .. ' %*'
+          end
+        end
+
+        local sep_l = ''
+        local sep_r = '%#St_sep_r#' .. '' .. ' %#ST_EmptySpace#'
+        return '%#St_harpoon_sep#' .. sep_l .. '%#St_harpoon_icon#' .. icon .. ' ' .. label .. sep_r
+      end,
+    },
   },
 
   telescope = {
--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -37,33 +37,10 @@ return {
             return labels
           end
 
-          local function get_harpoon_items()
-            local harpoon = require 'harpoon'
-            local marks = harpoon:list().items
-            local current_file_path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':.')
-            local label = {}
-
-            for id, item in ipairs(marks) do
-              if item.value == current_file_path then
-                table.insert(label, { id .. ' ', guifg = '#FFFFFF', gui = 'bold' })
-              else
-                table.insert(label, { id .. ' ', guifg = '#434852' })
-              end
-            end
-
-            if #label > 0 then
-              table.insert(label, 1, { '󰛢 ', guifg = '#61AfEf' })
-              table.insert(label, { '| ' })
-            end
-            return label
-          end
-
           return {
             { get_git_diff() },
-            { get_harpoon_items() },
             { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
             { filename .. ' ', gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold' },
-            { '|  ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
           }
         end,
         window = {
