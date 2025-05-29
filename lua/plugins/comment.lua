b69f9f8b7b16e78ae22408c6cdd9742b060f5603
Vu Nguyen <vunguyen4920@gmail.com>
2025-05-29 08:07:22 +0700

    amen

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -17,7 +17,7 @@ return {
       {
         'JoosepAlviste/nvim-ts-context-commentstring',
         opts = {},
-        config = function()
+        init = function()
           vim.g.skip_ts_context_commentstring_module = true
         end,
         event = 'BufReadPost',
