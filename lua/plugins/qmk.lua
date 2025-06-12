11eb7c6530f0dcd44632a12d89e4efdaa7306822
Vu Nguyen <vunguyen4920@gmail.com>
2025-06-13 00:41:48 +0700

    amen

--- lua/plugins/qmk.lua
+++ lua/plugins/qmk.lua
@@ -9,36 +9,48 @@ return {
       auto_format_pattern = '*.keymap',
       comment_preview = {
         keymap_overrides = {
+          ['&none'] = '∅',
           ['&trans'] = '',
           ['&sys_reset'] = '🔄',
           ['&bootloader'] = '💾',
           ['&studio_unlock'] = '🔓',
-          ['&bt '] = '🛜',
-          BT_NXT = '🔼',
-          BT_PRV = '🔽',
-          BT_CLR = '❌',
-          BT_SEL = '',
+          ['&caps_word'] = '⇪',
+          PRINTSCREEN = '🖨️',
+          BT_NXT = '🛜🔼',
+          BT_PRV = '🛜🔽',
+          BT_CLR = '🛜❌',
+          BT_SEL = '🛜',
           C_MUTE = '🔇',
           C_VOL_DN = '🔉',
           C_VOL_UP = '🔊',
           C_BRI_UP = '🔆',
           C_BRI_DN = '🔅',
-          ['&ext_power EP_TOG'] = '🔌',
+          EP_TOG = '🔌',
           AMPS = '&',
           STAR = '*',
           LPAR = '(',
           RPAR = ')',
           MEH = 'MEH',
           HYPER = 'HYPER',
+          TAB = '⇥',
+          RET = '⏎',
+          RSHIFT = '⇧',
+          LSHIFT = '⇧',
+          LG = '⌘',
+          RG = '⌘',
+          LC = '⌃',
+          RC = '⌃',
+          LA = '⌥',
+          RA = '⌥',
           ['COPY'] = '📄',
           ['PASTE'] = '📋',
           ['CUT'] = '✂️',
-          ['LG(Q)'] = '⌘Q',
-          ['RG(W)'] = '⌘W',
-          ['LC(W)'] = '⌃W',
-          ['LC(TAB)'] = '⌃⇥',
           SPACE = '␣',
-          KP_MULTIPLY = '*',
+          BSPC = '⌫',
+          DEL = '⌦',
+          ESC = '⎋',
+          LALT = '⌥',
+          RALT = '⌥',
           -- use MENU as compose key
           K_CMENU = '🌍',
           K_MENU = '🌍',
@@ -53,14 +65,15 @@ return {
           PG_DN = '⇟',
           HOME = '⇱',
           END = '⇲',
+
           -- mouse stuff
           MB1 = '🖱️L',
           MB2 = '🖱️R',
           MB3 = '🖱️M',
           MB4 = '🖱️B4',
           MB5 = '🖱️F5',
-          MOVE_X = '🖱️X',
-          MOVE_Y = '🖱Y',
+          MOVE_X = '🖱️↔️', -- or 'X' or '➡️⬅️'
+          MOVE_Y = '🖱️↕️', -- or 'Y' or '⬆️⬇️'
           MOUSE_MOVE_GEAR1_VAL = '1',
           MOUSE_MOVE_GEAR2_VAL = '2',
           MOUSE_MOVE_GEAR3_VAL = '3',
@@ -78,5 +91,36 @@ return {
         '_ _ _ x x x _ _ x x x _ _ _',
       },
     },
+    config = function(_, opts)
+      ---@param keymap qmk.KeymapList
+      package.loaded['qmk.format.get_key_text'] = function(keymap)
+        ---@type table<string, string>
+        local mapped = {}
+        for _, k in ipairs(keymap) do
+          mapped[k.key] = k.value
+        end
+        for _, v in ipairs { 'GUI', 'ALT', 'CTRL', 'SHIFT' } do
+          mapped['R' .. v] = 'R' .. v
+          mapped['L' .. v] = 'L' .. v
+        end
+        return function(key)
+          local parts = vim.split(key, ' ')
+          if vim.tbl_contains({ '&kp', '&bt', '&ext_power', '&msc', '&mmv', '&mkp' }, parts[1]) then
+            table.remove(parts, 1)
+          end
+          for i, part in ipairs(parts) do
+            part = mapped[part] or mapped['KC_' .. part] or part
+            part = part:gsub('^N(%d)$', '%1')
+            if part:find '&' ~= 1 then
+              part = part:upper()
+            end
+            parts[i] = part
+          end
+          return table.concat(parts, ' ')
+        end
+      end
+
+      require('qmk').setup(opts)
+    end,
   },
 }
