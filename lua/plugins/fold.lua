621c60ed18dd945285c7a1e626d90b09033c3082
Vu Nguyen <vunguyen4920@gmail.com>
2025-06-21 12:29:43 +0700

    feat(fold): no mo ufo

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -1,31 +1,11 @@
 return {
   {
-    'kevinhwang91/nvim-ufo',
-    event = 'BufReadPost',
-    dependencies = {
-      'kevinhwang91/promise-async',
-    },
-    opts = {},
-    config = function()
-      require('ufo').setup {
-        provider_selector = function()
-          return { 'treesitter', 'indent' }
-        end,
-        close_fold_kinds_for_ft = {
-          default = { 'imports', 'comment' },
-        },
-      }
-
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-      vim.o.foldcolumn = '1' -- '0' is not bad
-      vim.o.foldlevel = 999 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 999
-      vim.o.foldenable = true
-    end,
-  },
-  {
     'chrisgrieser/nvim-origami',
     event = 'VeryLazy',
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
     opts = {}, -- needed even when using default config
   },
 }
